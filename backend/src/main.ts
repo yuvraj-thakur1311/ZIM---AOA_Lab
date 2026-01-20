c458f60b68e68b09a3cd134dc79f42bb5ed87ce3
Shashankmishra227 <shashankmishra14433@gmail.com>
2026-01-20 10:54:24 +0530

    Added cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'], // Vite dev server
+    origin: "*", // Vite dev server
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
